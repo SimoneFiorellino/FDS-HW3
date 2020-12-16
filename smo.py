60622bc55d23972c38b9cd6e64cfca8cf9d2c67c
stdrr <development.stdrr@gmail.com>
2020-12-16 23:28:08 +0100

    Code cleaning

--- smo.py
+++ smo.py
@@ -1,84 +1,31 @@
- 
-
-# def take_step(i1, i2, alpha, y, placeholder_svm, C, x):
-#     if i1 == i2:
-#         return 0
-
-#     alph1 = alpha[i1]
-#     y1 = y[i1]
-#     E1 = placeholder_svm(i1) - y1
-#     s = y1 * y[i2]
-#     if y1 != y[i2]:
-#         L = max(0, alph1 - alpha[i2])
-#         H = min(C, C + alpha[i2] - alph1)
-#     else:
-#         L = max(0, alpha[i2] + alph1 - C)
-#         H = min(C, alpha[i2] - alph1)
-#     if L == H:
-#         return 0
-#     k11 = self.kernel(x[i1], x[i1])
-#     k12 = self.kernel(x[i1], x[i2])
-#     k22 = self.kernel(x[i2], x[i2])
-#     eta = k11 + k22 - 2 * k12
-#     if eta > 0: 
-#         a2 = alpha[i2] + y[i2] * (E1 - E2) / eta
-#         if a2 < L:
-#             a2 = L
-#         elif a2 > H:
-#             a2 = H
-#     else:
-#         Lobj = objective_f(L)
-#         Hobj = objective_f(H)
-#         if Lobj < Hobj - eps:
-#             a2 = L
-#         elif Lobj > Hobj + eps:
-#             a2 = H
-#         else:
-#             a2 = alpha[i2]
-#     if abs(a2 - alpha[i2] < eps * (a2 + alpha[i2] + eps)):
-#         return 0
-#     a1 = alpha[i1] + s * (alpha[i2] - a2)
-#     if 0 < a1 < C:
-#         b = b - E1 - y1 * (a1 - alpha[i1]) * self.kernel(x[i1], x[i1]) - y[i2] * (a2 - alpha[i2]) * self.kernel(x[i1], x[i2])
-#     elif 0 < a2 < C:
-#         b = b - E2 - y1 * (a1 - alpha[i1]) * self.kernel(x[i1], x[i2]) - y[i2] * (a2 - alpha[i2]) * self.kernel(x[i2], x[i2])
-#     else:
-#         if L != H:
-#            b1 = b - E1 - y1 * (a1 - alpha[i1]) * self.kernel(x[i1], x[i1]) - y[i2] * (a2 - alpha[i2]) * self.kernel(x[i1], x[i2]) 
-#            b2 = b - E2 - y1 * (a1 - alpha[i1]) * self.kernel(x[i1], x[i2]) - y[i2] * (a2 - alpha[i2]) * self.kernel(x[i2], x[i2])
-#            b = (b1 + b2) / 2
-#     E1 = placeholder_svm(i1) - y1
-#     E2 = placeholder_svm(i2) - y[i2]
-#     alpha[i1] = a1
-#     alpha[i2] = a2
-#     return 1
-
-
-# def examine_example(i2):
-#     y2 = y[i2]
-#     alph2 = alpha[i2]
-#     E2 = placeholder_svm(i2) - y2
-#     r2 = E2 * y2
-#     if (r2 < -tol and alph2 < C) or (r2 > tol and alph2 > 0):
-#         if n_non_zero_C > 1: 
-#             i1 = 
-
-
 import numpy as np
+import pandas as pd # lets us handle data as dataframes
+
+
 
 
 class SVM:
     
-    def __init__(self, x, y):
+    def __init__(self, x, y, C=1, tol=0.001, eps=0.001, kernel='linear', sigma=0.45):
         self.x = x
         self.y = y
         self.alpha = None
-        self.b = None
+        self.b = 0
         self.m = x.shape[0]
+        self.C = C
+        self.tol = tol
+        self.eps = eps
+        self.simga = sigma
+        kernel_type = {
+            'linear': lambda x_i, x_j: np.sum(np.dot(x_i, x_j)),
+            'gaussian': lambda x_i, x_j: np.exp(- np.square(np.linalg.norm(x_i - x_j)) / 2*(sigma**2))
+        }
+        self.kernel = kernel_type[kernel]
 
 
-    def kernel(self, x_i, x_j):
-        return np.sum(np.dot(x_i, x_j))
+
+    # def kernel(self, x_i, x_j):
+    #     return np.sum(np.dot(x_i, x_j))
 
 
     def gamma(self, u):
@@ -87,6 +34,16 @@ class SVM:
         for i in range(0, m):
             result += self.alpha[i] * self.y[i] * self.kernel(self.x[i], u)
         return result + self.b
+
+
+    def objective_function(self):
+        alpha_non_zero = np.flatnonzero(self.alpha != 0)
+        result = 0
+        for i in alpha_non_zero:
+            for j in alpha_non_zero:
+                result += self.y[i] * self.y[j] * self.alpha[i] * self.alpha[j] * self.kernel(self.x[i], self.x[j])
+        result -= np.sum(self.alpha[alpha_non_zero])
+        return result
 
 
     def _error(self, x_i, y_i):
@@ -174,9 +131,9 @@ class SVM:
         else:
             aj_old = self.alpha[j]
             self.alpha[j] = L
-            Lobj = self.gamma(self.x[j, :])
+            Lobj = self.objective_function()
             self.alpha[j] = H
-            Hobj = self.gamma(self.x[j, :])
+            Hobj = self.objective_function()
             self.alpha[j] = aj_old
             if Lobj < Hobj - eps:
                 aj_new = L
@@ -188,12 +145,12 @@ class SVM:
             return False
         ai_new = self.alpha[i] + s * (self.alpha[j] - aj_new)
         if 0 < ai_new < C:
-            self.b = self.b + self.E[i] + self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[i]) +  self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[i], self.x[j])
+            self.b = self.b - self.E[i] - self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[i]) - self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[i], self.x[j])
         elif 0 < aj_new < C:
-            self.b = self.b + self.E[j] + self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[j]) +  self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[j], self.x[j])
+            self.b = self.b - self.E[j] + self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[j]) - self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[j], self.x[j])
         else:
-            b1 = self.b + self.E[i] + self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[i]) +  self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[i], self.x[j])
-            b2 = self.b + self.E[j] + self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[j]) +  self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[j], self.x[j])
+            b1 = self.b - self.E[i] - self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[i]) - self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[i], self.x[j])
+            b2 = self.b - self.E[j] - self.y[i] * (ai_new - self.alpha[i]) * self.kernel(self.x[i], self.x[j]) - self.y[j] * (aj_new - self.alpha[j]) * self.kernel(self.x[j], self.x[j])
             self.b = (b1 + b2) / 2
         self.E[i] = self._error(self.x[i], self.y[i])
         self.E[j] = self._error(self.x[j], self.y[j])
@@ -211,7 +168,7 @@ class SVM:
             ex_not_bounds = self._check_bounds(C)
             ex_not_bounds_len =  len(ex_not_bounds)
             if ex_not_bounds_len > 1:
-                i = self._choose_i(j)
+                i = self._choose_i(j, ex_not_bounds)
                 if self.take_step(i, j, C, eps):
                     return 1
                 random_point = np.random.randint(0, ex_not_bounds_len)
@@ -219,31 +176,35 @@ class SVM:
                     i = (k + random_point) % ex_not_bounds_len
                     if self.take_step(i, j, C, eps):
                         return 1
-                random_point = np.random.randint(0, self.m)
-                for k in range(0, self.m):
-                    i = (k + random_point) % ex_not_bounds_len
-                    if self.take_step(i, j, C, eps):
-                        return 1
+            random_point = np.random.randint(0, self.m)
+            for k in range(0, self.m):
+                i = (k + random_point) % self.m
+                if self.take_step(i, j, C, eps):
+                    return 1
         return 0
 
 
-    def _choose_i(self, j):
-        return 1
+    def _choose_i(self, j, non_bounds):
+        if self.E[j] > 0:
+            return np.argmin(self.E[non_bounds])
+        return np.argmax(self.E[non_bounds])
 
 
     def _check_bounds(self, C): # first heuristic
         return np.flatnonzero(np.logical_and(self.alpha > 0, self.alpha < C))
 
 
-    def complete_smo(self, C, tol, eps, max_passes):
-        self.E = self.y * -1
+    def complete_smo(self, C, tol, eps):
         m = self.x.shape[0]
+        self.E = self.y * -1
+        self.alpha = np.zeros((m, 1))
+        self.b = 0
         num_changed = 0 
         examine_all = True
         while num_changed > 0 or examine_all:
             num_changed = 0
             if examine_all:
-                for i in m:
+                for i in range(m):
                     num_changed += self._examine_example(i, C, tol, eps)
             else: # first heuristic
                 ex_not_bounds = self._check_bounds(C) # array of indexes
@@ -256,50 +217,44 @@ class SVM:
         return self.alpha, self.b
 
 
-import numpy as np # imports a fast numerical programming library
-import scipy as sp # imports stats functions, amongst other things
-import matplotlib as mpl # this actually imports matplotlib
-import matplotlib.cm as cm # allows us easy access to colormaps
-import matplotlib.pyplot as plt # sets up plotting under plt
-import pandas as pd # lets us handle data as dataframes
 
-# sets up pandas table display
-pd.set_option('display.width', 500)
-pd.set_option('display.max_columns', 100)
-pd.set_option('display.notebook_repr_html', True)
-
-import seaborn as sns # sets up styles and gives us more plotting options
-
-
-df_x = pd.read_csv("./data/logistic_x.txt", sep="\ +", names=["x1","x2"], header=None, engine='python')
-df_y = pd.read_csv('./data/logistic_y.txt', sep='\ +', names=["y"], header=None, engine='python')
+df_x = pd.read_csv("./data/logistic_x.txt", sep=" +", names=["x1","x2"], header=None, engine='python')
+df_y = pd.read_csv('./data/logistic_y.txt', sep=' +', names=["y"], header=None, engine='python')
 df_y = df_y.astype(int)
 print(df_x.head())
 
 x = np.hstack([np.ones((df_x.shape[0], 1)), df_x[["x1","x2"]].values])
 y = df_y["y"].values
 
+# np.random.seed(17349)
 
-svm = SVM(x, y)
+svm = SVM(x, y, kernel='linear')
 
-alpha, b = svm.simplified_smo(C=1, tol=0.00001, eps=0.00001, max_passes=50)
+alpha, b = svm.complete_smo(C=1, tol=0.001, eps=0.001)
+print()
 print(alpha)
 print()
 print(b)
+print()
+print(svm.gamma(x[5]))
+# alpha, b = svm.simplified_smo(C=1, tol=0.00001, eps=0.00001, max_passes=50)
+# print(alpha)
+# print()
+# print(b)
 
-print(x[98, :])
-print(y[98])
+# print(x[98, :])
+# print(y[98])
 
-print(svm.gamma(x[98]))
+# print(svm.gamma(x[98]))
 
 
 
 from sklearn.svm import SVC
 
 
-svc = SVC(kernel='linear', tol=0.00001, max_iter=50)
+svc = SVC(kernel='linear', tol=0.001, max_iter=300)
 svc.fit(x, y)
-test = x[98]
+test = x[5]
 test = test.reshape(1, -1)
 print(svc.predict(test))
 print(svc.decision_function(test))
